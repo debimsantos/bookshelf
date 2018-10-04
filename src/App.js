04179514dae96d1e87a9b1368034372d571e85eb
debimsantos <debimortola@gmail.com>
2018-10-03 22:52:18 -0700

    Book component and mainpage rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import MainPage from './MainPage.js';
 import SearchPage from './SearchPage.js';
 import './App.css'
 
 class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books: books })
+    })
+  }
+
   render() {
     return(
       <div className='app'>
-        <MainPage />
+        <MainPage
+          books={this.state.books}
+        />
       </div>
       );
   }
